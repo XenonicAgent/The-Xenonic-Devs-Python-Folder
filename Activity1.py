1011bd15063b265875785d1a99196236292a3a52
XenonicAgent <priti20861@gmail.com>
2025-09-17 18:43:13 +0530

    1st

--- Activity1.py
+++ Activity1.py
@@ -1,1 +1,13 @@
-print("hello,world")
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((4000, 2000))
+clock = pygame.time.clock()
+square_pos = pygame.Rect(2950, 1920, 50, 50)
+while True:
+    if pygame.event.get(pygame.QUIT):break
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+    square_pos.y -= 20
+    screen.fill("black")
+    pygame
+
