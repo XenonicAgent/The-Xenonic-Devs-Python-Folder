ce72ecb5893af004abdc1ebbaeab2c41839eaabe
XenonicAgent <priti20861@gmail.com>
2025-09-26 18:02:45 +0530

    1st

---
+++ Loops/Activity1.py
@@ -0,0 +1,6 @@
+# Python program to print the multiplication table 23 from 1
+# Loop from 1 to 10
+num = int(input("Enter number to print table "))
+for i in range(1, 11):
+# Print the muliplication of 23 by i
+    print(num, "X",i," = ",num*i)
